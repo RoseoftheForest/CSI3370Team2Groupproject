b31bc0de358a29f93afc80e8ac7e89ec3be02523
ClaireChilders <clairechilders@oakland.edu>
2023-11-09 11:02:27 -0500

    feat: add attributes

--- Adventuregame/server/src/adventuregame/Option.java
+++ Adventuregame/server/src/adventuregame/Option.java
@@ -1,7 +1,14 @@
 package adventuregame;
 
 public class Option {
+    String text;
+    int id;
+    Outcome outcome;
+    
     public Option() {
         
+    }
+    public int getID() {
+        return this.id;
     }
 }
