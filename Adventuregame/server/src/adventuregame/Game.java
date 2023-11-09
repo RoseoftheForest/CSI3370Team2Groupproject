e35b99d2cd009b00f1e14b58c27331876a82bb91
ClaireChilders <clairechilders@oakland.edu>
2023-11-09 10:56:47 -0500

    feat: add game options

--- Adventuregame/server/src/adventuregame/Game.java
+++ Adventuregame/server/src/adventuregame/Game.java
@@ -1,6 +1,36 @@
 package adventuregame;
 
 public class Game {
+    public enum SoundOption {
+        VOLUME10,
+        VOLUME9,
+        VOLUME8,
+        VOLUME7,
+        VOLUME6,
+        VOLUME5,
+        VOLUME4,
+        VOLUME3,
+        VOLUME2,
+        VOLUME1,
+        VOLUME0,
+    }
+    public enum TextSpeedOption {
+        INSTANT,
+        FAST,
+        NORMAL,
+        SLOW
+    }
+    public enum TextSizeOption {
+        LARGE,
+        MEDIUM,
+        SMALL
+    }
+    public enum BackgroundColor {
+        BLACK,
+        WHITE,
+        BLUE
+    }
+    
     public Game() {
         
     }
