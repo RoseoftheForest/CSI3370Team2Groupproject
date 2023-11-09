8fa17fffe58df38017bebd899b97e4896ebb6123
ClaireChilders <clairechilders@oakland.edu>
2023-11-09 11:04:32 -0500

    feat: add initial attributes

--- Adventuregame/server/src/adventuregame/Player.java
+++ Adventuregame/server/src/adventuregame/Player.java
@@ -1,7 +1,22 @@
 package adventuregame;
 
 public class Player {
-    public Player() {
+    String name;
+    String id;
+    Choice currentChoice;
+    Scores scores;
+    Settings settings;
+    double progress;
+    
+    public Player(String id) {
         
+    }
+
+    public void setChoice(Choice c) {
+        this.currentChoice = c;
+    }
+
+    public Scores getScores() {
+        return this.scores;
     }
 }
