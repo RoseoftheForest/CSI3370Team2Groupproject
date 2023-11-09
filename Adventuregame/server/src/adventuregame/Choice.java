cd733af75e3a0d925ae225ad69bc4ef86483c5c1
ClaireChilders <clairechilders@oakland.edu>
2023-11-09 11:03:25 -0500

    feat: add option functions

--- Adventuregame/server/src/adventuregame/Choice.java
+++ Adventuregame/server/src/adventuregame/Choice.java
@@ -6,6 +6,18 @@ public class Choice {
     Option[] options;
 
     public Choice() {
-        
+
+    }
+
+    public void selectOption(Player p, int optionID) {
+        getSelectedoption(optionID).makeSelection(p);
+    }
+    public Option getSelectedoption(int optionID) {
+        for (int i = 0; i < options.length; i++) {
+            if (options[i].getID() == optionID) {
+                return options[i];
+            }
+        }
+        return null;
     }
 }
