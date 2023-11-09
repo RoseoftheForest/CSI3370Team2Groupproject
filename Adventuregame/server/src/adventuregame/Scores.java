7b1410d106349dfe7e4cfec127716ac3f44701d8
ClaireChilders <clairechilders@oakland.edu>
2023-11-09 10:58:09 -0500

    feat: define scores attributes

--- Adventuregame/server/src/adventuregame/Scores.java
+++ Adventuregame/server/src/adventuregame/Scores.java
@@ -1,6 +1,15 @@
 package adventuregame;
 
 public class Scores {
+    double[] values;
+    public enum Attribute {
+        HEALTH,
+        STRENGTH,
+        GOOD,
+        BAD,
+        NEUTRAL
+    }
+
     public Scores() {
         
     }
