fb21f2518c37a7b8ab5c1920511f7118aaf42903
ClaireChilders <clairechilders@oakland.edu>
2023-11-20 16:12:22 -0500

    fix: require defense not 0
    avoid divide by 0 errors in damage calculations while maintaining simplicity of constructors

--- server/src/adventuregame/Stats.java
+++ server/src/adventuregame/Stats.java
@@ -12,23 +12,37 @@ public class Stats {
     public Stats() {
         this.baseAtk = 0;
         this.abilityAtk = 0;
-        this.baseDef = 0;
-        this.abilityDef = 0;
+        this.baseDef = 1;
+        this.abilityDef = 1;
         this.max_health = 0;
     }
     public Stats(int value) {
+        if (value == 0) {
+            this.baseDef = 1;
+            this.abilityDef = 1;
+        } else {
+            this.baseDef = value;
+            this.abilityDef = value;
+        }
+        
         this.baseAtk = value;
         this.abilityAtk = value;
-        this.baseDef = value;
-        this.abilityDef = value;
         this.max_health = value;
     }
-    public Stats(int phy_atk, int mgc_atk, int phy_def, int mgc_def, int max_health) {
-        this.baseAtk = phy_atk;
-        this.abilityAtk = mgc_atk;
-        this.baseDef = phy_def;
-        this.abilityDef = mgc_def;
-        this.max_health = max_health;
+    public Stats(int baseAtk, int abilityAtk, int baseDef, int abilityDef, int maxHealth) {
+        this.baseAtk = baseAtk;
+        this.abilityAtk = abilityAtk;
+        if (baseDef == 0) {
+            this.baseDef = 1;
+        } else {
+            this.baseDef = baseDef;
+        }
+        if (abilityDef == 0) {
+            this.abilityDef = 1;
+        } else {
+            this.abilityDef = abilityDef;
+        }
+        this.max_health = maxHealth;
     }
 
     public int getBaseAtk() {
