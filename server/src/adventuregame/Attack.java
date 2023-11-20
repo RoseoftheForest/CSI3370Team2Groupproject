5860dee3ea01341fb968ec102a7e6e2475c7af65
ClaireChilders <clairechilders@oakland.edu>
2023-11-20 16:13:56 -0500

    feat: add random damage variation

--- server/src/adventuregame/Attack.java
+++ server/src/adventuregame/Attack.java
@@ -1,14 +1,23 @@
 package adventuregame;
 
+import java.util.Random;
+
 public class Attack {
     private double baseDamage;
+    private int baseVariation; // 0 to 100
     private double abilityDamage;
+    private int abilityVariation; // 0 to 100
     private double trueDamage;
+    private Random random = new Random();
+    private static double DAMAGE_MULTIPLIER = 5.0;
+
 
     public Attack(double baseDamage, double abilityDamage, double trueDamage) {
         this.baseDamage = baseDamage;
         this.abilityDamage = abilityDamage;
         this.trueDamage = trueDamage;
+        this.baseVariation = 0;
+        this.abilityVariation = 0;
     }
 
     public void setBaseDamage(double damage) {
@@ -20,12 +29,24 @@ public class Attack {
     public void setTrueDamage(double damage) {
         this.trueDamage = damage;
     }
+    public void setBaseVariation(int variation) {
+        this.baseVariation = variation;
+    }
+    public void setAbilityVariation(int variation) {
+        this.abilityVariation = variation;
+    }
 
     public double getBaseDamage() {
-        return this.baseDamage;
+        return (this.baseDamage) * getBaseVariation();
+    }
+    private double getBaseVariation() {
+        return (random.nextInt(-baseVariation, baseVariation) + 100) / 100.0;
+    }
+    private double getAbilityVariation() {
+        return (random.nextInt(-abilityVariation, abilityVariation) + 100) / 100.0;
     }
     public double getAbilityDamage() {
-        return this.abilityDamage;
+        return (this.abilityDamage) * getAbilityVariation();
     }
     public double getTrueDamage() {
         return this.trueDamage;
