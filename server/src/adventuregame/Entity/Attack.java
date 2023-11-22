225e3568d8907a3e6d3e7d2d3fd4628812ce89ca
ClaireChilders <clairechilders@oakland.edu>
2023-11-22 15:49:47 -0500

    feat: remove ability damage

--- server/src/adventuregame/Entity/Attack.java
+++ server/src/adventuregame/Entity/Attack.java
@@ -5,8 +5,6 @@ import java.util.Random;
 public class Attack {
     private double baseDamage;
     private int baseVariation; // 0 to 100
-    private double abilityDamage;
-    private int abilityVariation; // 0 to 100
     private double trueDamage;
     private Random random = new Random();
     private static double DAMAGE_MULTIPLIER = 5.0;
@@ -17,14 +15,10 @@ public class Attack {
         this.abilityDamage = abilityDamage;
         this.trueDamage = trueDamage;
         this.baseVariation = 0;
-        this.abilityVariation = 0;
     }
 
     public void setBaseDamage(double damage) {
         this.baseDamage = damage;
-    }
-    public void setAbilityDamage(double damage) {
-        this.abilityDamage = damage;
     }
     public void setTrueDamage(double damage) {
         this.trueDamage = damage;
@@ -32,8 +26,6 @@ public class Attack {
     public void setBaseVariation(int variation) {
         this.baseVariation = variation;
     }
-    public void setAbilityVariation(int variation) {
-        this.abilityVariation = variation;
     }
 
     public double getBaseDamage() {
@@ -42,11 +34,6 @@ public class Attack {
     private double getBaseVariation() {
         return (random.nextInt(-baseVariation, baseVariation) + 100) / 100.0;
     }
-    private double getAbilityVariation() {
-        return (random.nextInt(-abilityVariation, abilityVariation) + 100) / 100.0;
-    }
-    public double getAbilityDamage() {
-        return (this.abilityDamage) * getAbilityVariation();
     }
     public double getTrueDamage() {
         return this.trueDamage;
@@ -54,10 +41,9 @@ public class Attack {
 
     public int getDamage(Stats attackerStats, Stats defenderStats) {
         double b_dmg = (getBaseDamage() * attackerStats.getBaseAtk()) / defenderStats.getBaseDef();
-        double a_dmg = (getAbilityDamage() * attackerStats.getAbilityAtk()) / defenderStats.getAbilityDef();
         double t_dmg = (getTrueDamage());
         
-        int dmg = (int)(DAMAGE_MULTIPLIER * (b_dmg + a_dmg + t_dmg));
+        int dmg = (int)(DAMAGE_MULTIPLIER * (b_dmg + t_dmg));
         return dmg;
     }
     public void useAttack(Entity attacker, Entity defender) {
