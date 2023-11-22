80a20b517d23f8def9ab5eb31b7bed37220cbd79
ClaireChilders <clairechilders@oakland.edu>
2023-11-21 23:35:47 -0500

    fix: remove duplicate header/footer

--- client/src/views/pages/Home.js
+++ client/src/views/pages/Home.js
@@ -1,13 +1,8 @@
 import React, { Fragment } from "react";
 
-import Header from "../../components/Header";
-import Footer from "../../components/Footer";
-
 const Home = () => (
     <Fragment>
-        <Header />
         <hr />
-        <Footer />
     </Fragment>
 )
 
