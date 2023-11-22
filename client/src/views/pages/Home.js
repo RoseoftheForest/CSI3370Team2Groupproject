ebc2db3942846e420ee55a49c19431384f9c1288
ClaireChilders <clairechilders@oakland.edu>
2023-11-21 22:53:04 -0500

    feat: create Home.js

---
+++ client/src/views/pages/Home.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react";
+
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+
+const Home = () => (
+    <Fragment>
+        <Header />
+        <hr />
+        <Footer />
+    </Fragment>
+)
+
+export default Home;
