50dba1b83501e77408995952500ad79fb5bf4b5d
ClaireChilders <clairechilders@oakland.edu>
2023-11-24 10:09:52 -0500

    feat: add quantity

--- server/src/adventuregame/Item/ShopItem.java
+++ server/src/adventuregame/Item/ShopItem.java
@@ -4,14 +4,23 @@ import adventuregame.Entity.Stats;
 
 public class ShopItem extends Item {
     private int cost;
+    private int quantity;
 
     public ShopItem(int id, String name, String description, Stats modifiers, int cost) {
         super(id, name, description, modifiers);
         this.cost = cost;
+        this.quantity = 1;
     }
 
     public int getCost() {
         return this.cost;
+    }
+    public int getQuantity() {
+        return this.quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
     }
     
 }
