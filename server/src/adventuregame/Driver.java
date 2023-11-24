aff6abc1752c774c4548f7b080f57b01d6e84171
ClaireChilders <clairechilders@oakland.edu>
2023-11-24 10:19:37 -0500

    refactor: update implementation

--- server/src/adventuregame/Driver.java
+++ server/src/adventuregame/Driver.java
@@ -6,7 +6,6 @@ import adventuregame.Room.FightRoom;
 
 public class Driver {
     
-    
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         Game game = Game.instance();
@@ -25,19 +24,19 @@ public class Driver {
     }
 
     public static void fightRoom(Scanner scan) {
+        //Scanner scan = new Scanner(System.in);
         Game game = Game.instance();
         System.out.println(game);
         FightRoom room = (FightRoom) game.getPlayer(1).getCurrentRoom();
         System.out.println(room.getMonster().getName() + " : " + room.getMonster().getHealth() + " / " + room.getMonster().getStats().getMaxHealth());
         System.out.println(game.getPlayer(1).getName() + " : " + game.getPlayer(1).getHealth() + " / " + game.getPlayer(1).getStats().getMaxHealth());
-        scan = new Scanner(System.in);
         while (game.getPlayer(1).isAlive() && room.getMonster().isAlive()) {
             System.out.println("Use special attack? Enter 1: ");
             int input = scan.nextInt();
             if (input == 1) {
-                game.specialAttack(1);
+                game.useSpecialAttack(1);
             } else {
-                game.basicAttack(1);
+                game.useBasicAttack(1);
             }
             System.out.println(room.getMonster().getName() + " : " + room.getMonster().getHealth() + " / " + room.getMonster().getStats().getMaxHealth());
             System.out.println(game.getPlayer(1).getName() + " : " + game.getPlayer(1).getHealth() + " / " + game.getPlayer(1).getStats().getMaxHealth());
