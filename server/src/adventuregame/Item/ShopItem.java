235dad85e78bb9b14512a7d124d08e42db277308
ClaireChilders <clairechilders@oakland.edu>
2023-11-24 10:10:03 -0500

    feat: add buy function

--- server/src/adventuregame/Item/ShopItem.java
+++ server/src/adventuregame/Item/ShopItem.java
@@ -1,5 +1,6 @@
 package adventuregame.Item;
 
+import adventuregame.Response;
 import adventuregame.Entity.Stats;
 
 public class ShopItem extends Item {
@@ -21,6 +22,20 @@ public class ShopItem extends Item {
 
     public void setQuantity(int quantity) {
         this.quantity = quantity;
+    }
+
+    public Response buy() {
+        Response response = new Response();
+        if (getQuantity() < 1) {
+            response.addMessage("There are no more " + getName() + " left!");
+            response.setResult(false);
+            return response;
+        }
+        setQuantity(getQuantity()-1);
+        response.addMessage("You purchased a " + getName());
+        response.addMessage("You gained " + stringifyModifiers());
+        response.setResult(true);
+        return response;
     }
     
 }
