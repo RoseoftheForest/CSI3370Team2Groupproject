7862a25e9eae6a6c716751725558d815207a6c2c
ClaireChilders <clairechilders@oakland.edu>
2023-11-26 16:37:30 -0500

    feat: add heal action

--- server/src/adventuregame/Response.java
+++ server/src/adventuregame/Response.java
@@ -8,7 +8,7 @@ public class Response {
     private Action nextAction;
 
     public static enum Action {
-        GAME_OVER, ATTACK, NEXT_ROOM, SHOP
+        GAME_OVER, ATTACK, NEXT_ROOM, SHOP, HEAL
     }
 
     public Response() {
