e8de0b1f414bf1d3f6bda678256a8157427f8a1f
ClaireChilders <clairechilders@oakland.edu>
2023-11-27 01:02:36 -0500

    Update GameController.java

--- server/src/adventuregame/GameController.java
+++ server/src/adventuregame/GameController.java
@@ -1,5 +1,6 @@
 package adventuregame;
 
+import adventuregame.Entity.Attack;
 import adventuregame.Entity.Player;
 import adventuregame.Response.Action;
 import adventuregame.Room.FightRoom;
@@ -7,73 +8,106 @@ import adventuregame.Room.Room;
 import adventuregame.Room.ShopRoom;
 
 public class GameController {
-    Game game;
-    GameView view;
+    private static Game game;
+    private GameView view;
+    private static GameController uniqueInstance;
 
-    public GameController() {
+    public static GameController instance() {
+        if (uniqueInstance == null) {
+            uniqueInstance = new GameController();
+        }
+        return uniqueInstance;
+    }
+
+    private GameController() {
         game = Game.instance();
-        view = new GameView();
+        view = GameView.instance();
+        view.setMenuScreen();
         view.start();
     }
+
+    public static void newPlayer(String name) {
+        int playerID = game.addPlayer(name);
+        startNewGame(playerID);
+    }
     
-    public void startNewGame(int playerID) {
+    public static void startNewGame(int playerID) {
+        instance().view.setPlayerID(playerID);
         Player p = game.getPlayer(playerID);
+        System.out.println("Received start new game interaction from " + p.getName() + " (" +  playerID + ")");
         p.reset();
-        game.nextRoom(p);
+        GameController.nextRoom(playerID);
+        System.out.println("Started new game for " + p.getName() + " (" +  playerID + ")");
+        
     }
 
-    public void useBasicAttack(int playerID) {
-        Player p = game.getPlayer(playerID);
-        Response response = game.useAttack(p, p.getBasicAttack());
-        view.displayMessage(response);
+    private static void useAttack(Player p, Attack a) {
+        Response response = game.useAttack(p, a);
+        instance().view.setFightRoom((FightRoom)p.getCurrentRoom());
+        if (response.getNextAction() == Action.ATTACK) {
+            
+        } else if (response.getNextAction() == Action.NEXT_ROOM) {
+            instance().view.setNextRoom();
+        } else if (response.getNextAction() == Action.GAME_OVER) {
+            instance().view.setGameOver();
+        }
+        instance().view.displayMessage(response);
     }
-    public void useSpecialAttack(int playerID) {
+    public static void useBasicAttack(int playerID) {
         Player p = game.getPlayer(playerID);
-        Response response = game.useAttack(p, p.getSpecialAttack());
-        view.displayMessage(response);
+        System.out.println("Received basic attack interaction from " + p.getName() + " (" +  playerID + ")");
+        useAttack(p, p.getBasicAttack());
+    }
+    public static void useSpecialAttack(int playerID) {
+        Player p = game.getPlayer(playerID);
+        System.out.println("Received special attack interaction from " + p.getName() + " (" +  playerID + ")");
+        useAttack(p, p.getSpecialAttack());
     }
     
-    public void buyItem(int playerID, int position) {
+    public static void buyItem(int playerID, int position) {
         Player p = game.getPlayer(playerID);
+        System.out.println("Received buy item interaction from " + p.getName() + " (" +  playerID + ")");
         if (p.getCurrentRoom().getClass() != ShopRoom.class) {
             return;
         }
         ShopRoom room = (ShopRoom) p.getCurrentRoom();
         
         Response response = p.buyItem(room.getItem(position));
-        view.displayMessage(response);
+        instance().view.displayMessage(response);
     }
     
-    public void nextRoom(int playerID) {
+    public static void nextRoom(int playerID) {
         Player p = game.getPlayer(playerID);
+        System.out.println("Received next room interaction from " + p.getName() + " (" +  playerID + ")");
         Response message = new Response();
         Response res = game.nextRoom(p);
+        System.out.println("Depth: " + p.getDepth());
         Room room = p.getCurrentRoom();
         if (res.getNextAction() == Action.ATTACK) {
             message.addMessage("This room contains a monster! You must defeat it to progress.");
-            view.setFightRoom((FightRoom)room);
+            instance().view.setFightRoom((FightRoom)room);
         } else if (res.getNextAction() == Action.SHOP) {
             message.addMessage("You stumbled upon a shop. Select any items you would like to buy or click Next Room to progress.");
-            view.setShopRoom((ShopRoom)room);
+            instance().view.setShopRoom((ShopRoom)room);
         } else if (res.getNextAction() == Action.HEAL) {
             message.addMessage("Suddenly, you feel revitalized. Click Next Room to progress.");
-            view.setHealRoom();
+            instance().view.setHealRoom();
         } else if (res.getNextAction() == Action.NEXT_ROOM) {
             message.addMessage("Click Next Room to progress.");
-            view.setNextRoom();
+            instance().view.setNextRoom();
         } else if (res.getNextAction() == Action.GAME_OVER) {
             message.addMessage("You have been defeated...");
-            view.setGameOver();
+            instance().view.setGameOver();
         } else {
             message.addMessage("error");
         }
-        view.displayMessage(message);
+        instance().view.displayMessage(message);
     }
     
     
 
     // TO BE IMPLEMENTED
-    public void selectSettings(int playerID, int settingID, int value) {
+    public static void selectSettings(int playerID, int settingID, int value) {
         Player p = game.getPlayer(playerID);
         Settings s = game.getSetting(settingID);
         s.setSetting(value);
@@ -82,7 +116,7 @@ public class GameController {
     
     // TO BE IMPLEMENTED
     // Need database functionality
-    public void logIn(int playerID) {
+    public static void logIn(int playerID) {
         Player p = game.getPlayer(playerID); // attempt to load player from existing memory
         if (p != null) {
             if (!p.isLoggedIn()) {
@@ -94,7 +128,7 @@ public class GameController {
         // otherwise, search database for player
         // otherwise, create new player, and save in database
     }
-    public void logOut(int playerID) {
+    public static void logOut(int playerID) {
         Player p = game.getPlayer(playerID);
         if (p == null || !p.isLoggedIn()) {return;} // player is not logged in
         p.logOut();
@@ -111,7 +145,7 @@ public class GameController {
         player.setStats(null);
         player.setCurrentRoom(game.getRoom(0));
     }
-    public void saveGame(int playerID) {
+    public static void saveGame(int playerID) {
         Player player = game.getPlayer(playerID);
         if (player == null) {
             return;
@@ -129,7 +163,7 @@ public class GameController {
 
     // TO BE IMPLEMENTED
     // Should return the default settings if the player hasn't modified the settings, or whatever their settings were if they have saved settings.
-    public void loadSettings(int playerID) {
+    public static void loadSettings(int playerID) {
         Player p = game.getPlayer(playerID);
         if (p == null) {return;}
         p.setSettings(new Settings());
